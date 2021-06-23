f9b67e81092687b12ab91339fcc6ef06f1e6d6b5
Khalil Mejdi <khalil.mejdi97@gmail.com>
2021-06-23 11:57:51 +0100

    Update HelloWorld.java

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -4,11 +4,12 @@ import java.util.Date;
 
 
 public class HelloWorld {
-	public static final String PASSWORD = "please work";
+	public static final String GF_DATABASE_PASSWORD = "PLEASE_FAIL";
 	public static void main(String[] args) {
 		Date currentTime = new Date();
 		System.out.println("The current local time is: " + currentTime);
 		Greeter greeter = new Greeter();
 		System.out.println(greeter.sayHello());
+		System.out.println(HelloWorld.GF_DATABASE_PASSWORD);
 	}
 }
